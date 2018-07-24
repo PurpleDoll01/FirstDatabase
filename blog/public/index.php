77cbc372e6311f23cc21c19aa69915445f263365
Mariana Valencia <purple.mv@gmail.com>
2018-07-23 20:23:59 -0500

    Introduccion a manejo de router libreria con primer index

--- blog/public/index.php
+++ blog/public/index.php
@@ -5,16 +5,25 @@ error_reporting(E_ALL);
 
 include_once '../config.php';
 
+require_once '../vendor/autoload.php'; // Con hacer esto ya podemos usar phroute en todos los archivos
+
 $route = $_GET['route'] ?? '/';
 
-switch($route) {
-    case '/':
-        require '../index.php';
-        break;
-    case '/admin':
-        require '../admin/index.php';
-        break;
-    case '/admin/posts':
-        require '../admin/posts.php';
-        break;
-}
+use Phroute\Phroute\RouteCollector;
+
+$router = new RouteCollector();
+$router->get('/', function() use ($pdo) {
+    $query = $pdo->prepare('SELECT * FROM blog_posts ORDER BY id DESC');
+    $query->execute();
+
+    $blogPosts = $query->fetchAll(PDO::FETCH_ASSOC);
+    include '../views/index.php';
+});
+
+$dispatcher = new Phroute\Phroute\Dispatcher($router->getData());
+$response = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], $route);
+
+echo $response;
+
+
+
--- blog/vendor/composer/autoload_static.php
+++ blog/vendor/composer/autoload_static.php
@@ -6,9 +6,25 @@ namespace Composer\Autoload;
 
 class ComposerStaticInitd24681ff4dfd0a41bef7023914d9447e
 {
+    public static $prefixLengthsPsr4 = array (
+        'P' => 
+        array (
+            'Phroute\\Phroute\\' => 16,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Phroute\\Phroute\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phroute/phroute/src/Phroute',
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInitd24681ff4dfd0a41bef7023914d9447e::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitd24681ff4dfd0a41bef7023914d9447e::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
