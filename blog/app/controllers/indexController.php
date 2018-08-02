864b074c2a84a60ca03b022e58206023684d363d
Mariana Valencia <purple.mv@gmail.com>
2018-08-01 22:01:52 -0500

    Paginacion y detalles de los posts

--- blog/app/controllers/indexController.php
+++ blog/app/controllers/indexController.php
@@ -6,8 +6,25 @@ use App\Models\BlogPost;
 
 class IndexController extends BaseController {
 
-    public function getIndex() {
-        $blogPosts = BlogPost::query()->orderBy('id', 'desc')->get();
-        return $this->render('index.twig', ['blogPosts' => $blogPosts]);
+    public function getIndex($page = 1){
+
+        $limit = 3;
+
+        $totalReg = BlogPost::all()->count();
+        $totalPages = ceil($totalReg / $limit);
+        $skip = ($limit * $page ) - $limit;
+
+        $blogPosts = BlogPost::query()->orderBy('id','desc')->skip($skip)->take($limit)->get();
+
+        return $this->render('index.twig', [
+        'blogPosts' => $blogPosts,
+        'totalPages' => $totalPages,
+        'page' => $page
+        ]);
+    }
+
+    public function getDetail($title) {
+        $blogPosts = BlogPost::where('title', $title)->get();
+        return $this->render('admin/detail.twig', ['blogPosts' => $blogPosts]);
     }
 }
